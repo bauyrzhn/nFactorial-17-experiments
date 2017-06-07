094de10204ec7c0ad00b302ee084d00732877539
Bauyrzhan Orynbassar <bauyrzhn@gmail.com>
2017-06-07 09:55:44 +0600

    bg changer

--- BG CHANGER/ViewController.swift
+++ BG CHANGER/ViewController.swift
@@ -10,6 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSun: UIButton!
+    @IBAction func Red(_ sender: Any) {
+        self.view.backgroundColor = UIColor(red:1.00, green:0.32, blue:0.18, alpha:1.0)
+        
+       redSun.setTitleColor(UIColor.white, for: .normal)
+    
+    }
+    
+    @IBOutlet weak var westHam: UIButton!
+    @IBAction func West(_ sender: Any) {
+        self.view.backgroundColor = UIColor(red:0.27, green:0.09, blue:0.73, alpha:1.0)
+        
+        westHam.setTitleColor(UIColor.white, for: .normal)
+    }
+    
+    
+    @IBOutlet weak var whiteKing: UIButton!
+    @IBAction func White(_ sender: Any) {
+        self.view.backgroundColor = UIColor(red:0.00, green:0.89, blue:0.74, alpha:1.0)
+    
+        whiteKing.setTitleColor(UIColor.white, for: .normal)
+        
+        
+     }
+    
+
+    @IBOutlet weak var numberhome: UIButton!
+    @IBAction func numberhome(_ sender: Any) {
+        self.view.backgroundColor = UIColor(red:1.00, green:1.00, blue:1.00, alpha:1.0)
+
+       numberhome.setTitleColor(UIColor.white, for: .normal)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
